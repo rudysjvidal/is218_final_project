a700618ecc72ed15b6b7aa2adfdfdc90d4cfda7f
Rudys J Vidal <rjv6@njit.edu>
2023-12-18 16:54:29 -0500

    added color file and function

---
+++ components/selectbycolor.js
@@ -0,0 +1,16 @@
+'use client';
+
+import React, { useState } from 'react';
+//import styles from '../src/app/styles/color.component.css';
+
+export default function Color() {
+
+    return (
+
+        <main id="colored">
+
+        </main>
+
+    );
+
+}
