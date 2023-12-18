57d0c1926d3186e013866e1f527b061f27200a89
Rudys J Vidal <rjv6@njit.edu>
2023-12-18 17:12:04 -0500

    add slogan text

--- components/slogan.js
+++ components/slogan.js
@@ -4,7 +4,10 @@
 export default function Slogan() {
     return (
         <main>
-
+            <div className="slogan-text">
+                <h3>Established in 1976</h3>
+                <h2>LESS IS MORE.</h2>
+            </div>
         </main>
     )
 }
