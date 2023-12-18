d6065fea8378f70b0b6254bf78495401825987ec
Rudys J Vidal <rjv6@njit.edu>
2023-12-18 16:44:48 -0500

    add womensclothing content

--- components/womens_clothing.js
+++ components/womens_clothing.js
@@ -5,6 +5,35 @@ export default function WomensClothing() {
     return (
         <main id="women">
 
+
+            <div className="shop-women">
+                <h3>Women</h3>
+            </div>
+
+
+            <div className="cards">
+                <div className="card1">
+                    <img src="/images/2.webp" alt="Image of woman tshirt" loading="lazy"></img>
+                    <p>Women’s Scoop Neck</p>
+                    <p>PlainShirts Tee</p>
+                </div>
+                <div className="card1">
+                    <img src="/images/1.webp" alt="Image of woman tshirt" loading="lazy"></img>
+                    <p>Women’s Long Sleeve</p>
+                    <p>PlainShirts Tee</p>
+                </div>
+                <div className="card1">
+                    <img src="/images/3.webp" alt="Image of woman tshirt" loading="lazy"></img>
+                    <p>Women’s V-Neck</p>
+                    <p>PlainShirts Tee</p>
+                </div>
+                <div className="card1">
+                    <img src="/images/4.webp" alt="Image of woman tshirt" loading="lazy"></img>
+                    <p>Women’s Crew Neck </p>
+                    <p>PlainShirts Tee</p>
+                </div>
+            </div>
+
         </main>
     )
 }
