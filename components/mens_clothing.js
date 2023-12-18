09aa93a6e87c425b3aca9a5837f9e10b3fefa6a4
Rudys J Vidal <rjv6@njit.edu>
2023-12-18 16:27:58 -0500

    add MensClothing text

--- components/mens_clothing.js
+++ components/mens_clothing.js
@@ -5,6 +5,46 @@ export default function MensClothing() {
     return (
         <main id="men">
 
+
+            <div className="shop-men">
+                <h3>men</h3>
+            </div>
+
+
+            <div className="cards">
+                <div className="card1">
+                    <img src="/images/8.webp" alt="Image of tshirt" loading="lazy"></img>
+                    <div>
+                        <p>Oversized Fit</p>
+                        <p>PlainShirts Tee</p>
+                    </div>
+                </div>
+                <div className="card1">
+                    <img src="/images/5.webp" alt="Image of tshirt" loading="lazy"></img>
+                    <div>
+
+                        <p>Crew Neck</p>
+                        <p>PlainShirts Tee</p>
+                    </div>
+                </div>
+                <div className="card1">
+                    <img src="/images/6.webp" alt="Image of tshirt" loading="lazy"></img>
+                    <div>
+
+                        <p>V-Neck</p>
+                        <p>PlainShirts Tee</p>
+                    </div>
+                </div>
+                <div className="card1">
+                    <img src="/images/7.webp" alt="Image of tshirt" loading="lazy"></img>
+                    <div>
+
+                        <p>Long Sleeve</p>
+                        <p>PlainShirts Tee</p>
+                    </div>
+                </div>
+            </div>
+
         </main>
     )
 }
