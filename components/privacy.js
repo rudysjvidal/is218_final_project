446a02a76739e16aa40e4db292e25f55d8338041
Rudys J Vidal <rjv6@njit.edu>
2023-12-18 18:58:08 -0500

    add privacy file and function

---
+++ components/privacy.js
@@ -0,0 +1,14 @@
+// PrivacyPolicy.js
+import React from 'react';
+//import styles from '../src/app/styles/terms.component.css';
+
+const PrivacyPolicy = () => {
+    return (
+        <div className="terms">
+
+
+        </div>
+    );
+};
+
+export default PrivacyPolicy;
