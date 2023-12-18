f49f3e5d7661d25c9c8bc9db67640d5905c1d598
Rudys J Vidal <rjv6@njit.edu>
2023-12-18 13:18:34 -0500

    imported and implemented Promtion Function

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 import Image from 'next/image'
+import Promotion from '../../components/promotion';
 
 export default function Home() {
   return (
     <main>
+      <Promotion />
     </main>
   )
 }
