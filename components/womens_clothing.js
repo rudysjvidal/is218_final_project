3a3eb148ebecc3992fab05c2b1d6cbec3e6a308e
Rudys J Vidal <rjv6@njit.edu>
2023-12-18 16:43:21 -0500

    add womenclothing file and function

---
+++ components/womens_clothing.js
@@ -0,0 +1,10 @@
+//import styles from '../src/app/styles/womensclothing.component.css';
+
+
+export default function WomensClothing() {
+    return (
+        <main id="women">
+
+        </main>
+    )
+}
