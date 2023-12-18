441103c06e13333feb1114545bab0812af77c016
Rudys J Vidal <rjv6@njit.edu>
2023-12-18 13:17:41 -0500

    added text to Promotion Function

--- components/promotion.js
+++ components/promotion.js
@@ -3,7 +3,12 @@
 export default function Promotion() {
     return (
         <main id="promotion">
-
+            <div className="promo">
+                <div className="promo-text">
+                    <p>BLACK FRIDAY DEAL: </p>
+                    <p className="red-text">&nbsp;&nbsp;30% OFF ALL ITEMS</p>
+                </div>
+            </div>
         </main>
     )
 }
