b23c523dd1da160e7eb06d08cfaf480b2150934d
Rudys J Vidal <rjv6@njit.edu>
2023-12-18 15:53:57 -0500

    add Hero content

--- components/hero.js
+++ components/hero.js
@@ -6,7 +6,18 @@ import React from "react";
 export default function Hero() {
     return (
         <main>
-
+            <div className="hero">
+                <div className="hero-image">
+                    <img className="hero-image-img" src="/images/twogreyshirts2.webp" alt="image of two people in plain shirts" loading="lazy"></img>
+                    <img className="hero-image-mobile-img" src="/images/twogreyshirtsvertical.webp" alt="image of two people in plain shirts" loading="lazy"></img>
+                </div>
+                <div className="hero-content">
+                    <div className="hero-text">
+                        <h1>ELEVATE YOUR ESSENTIALS</h1>
+                        <h3>EARN 30% OFF WHEN YOU SIGN UP</h3>
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
