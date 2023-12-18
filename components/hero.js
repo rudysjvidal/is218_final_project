3163b84623eaf8e61793a5f72f33d5b760e23d57
Rudys J Vidal <rjv6@njit.edu>
2023-12-18 15:48:07 -0500

    add hero file and function

---
+++ components/hero.js
@@ -0,0 +1,20 @@
+import styles from '../src/app/styles/hero.component.css';
+import React from "react";
+
+
+
+export default function Hero() {
+    return (
+        <main>
+
+        </main>
+    )
+}
+
+/*
+<a>
+<Button color="primary" radius="full" className="hero-button-2">
+    SIGN UP HERE
+</Button>
+</a> 
+*/
