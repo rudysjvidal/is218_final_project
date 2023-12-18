dc811d681d8c38c4e55cbf0390693e1fe99da630
Rudys J Vidal <rjv6@njit.edu>
2023-12-18 18:55:00 -0500

    add privacy folder and page file

--- src/app/privacy/page.js
+++ src/app/privacy/page.js
@@ -1,14 +1,12 @@
 
 import Navbar from '../../../components/navbar2';
-import TermsAndConditions from '../../../components/terms';
+//import PrivacyPolicy from '../../../components/privacy';
 import Footer from '../../../components/footer';
-
-
 export default function Home() {
     return (
         <main>
             <Navbar />
-            <TermsAndConditions />
+
             <Footer />
         </main>
     );
