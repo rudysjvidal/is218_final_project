e862022dc262354cabb869e7ec815f6198491a10
Rudys J Vidal <rjv6@njit.edu>
2023-12-18 17:24:32 -0500

    add footer file and function

---
+++ components/footer.js
@@ -0,0 +1,14 @@
+//import styles from '../src/app/styles/footer.component.css';
+
+
+import React from 'react';
+import Link from 'next/link'
+
+
+export default function Footer() {
+    return (
+        <main className="footer">
+            
+        </main >
+    );
+}
