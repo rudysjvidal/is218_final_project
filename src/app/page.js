168c6864f8c2322e57098a8671f5551f3703eb01
Rudys J Vidal <rjv6@njit.edu>
2023-12-18 15:43:49 -0500

    add styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Promotion from '../../components/promotion';
+import Navbar from '../../components/navbar'
 
 export default function Home() {
   return (
     <main>
       <Promotion />
+      <Navbar />
     </main>
   )
 }
