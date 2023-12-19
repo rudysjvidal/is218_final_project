84c1c0d2a85f6861f8e21e5fd27b8ac1b26075d6
Rudys J Vidal <rjv6@njit.edu>
2023-12-18 19:00:49 -0500

    add privacy content

--- components/privacy.js
+++ components/privacy.js
@@ -6,7 +6,41 @@ const PrivacyPolicy = () => {
     return (
         <div className="terms">
 
+            <div className={styles.privacyPolicy}>
+                <div className="mb-2 text-xl font-bold">PlainShirts - Privacy Policy</div>
+                <div>
+                    <p>Welcome to PlainShirts! Your privacy is as important to us as the plainness of our T-shirts. Please take a moment to familiarize yourself with our Privacy Policy.</p>
 
+                    <h2 className="text-lg font-bold mt-4">1. Information We Collect</h2>
+                    <p>1.1 <strong>Basic Shirt Preferences:</strong> When you visit our site, we may collect information about your favorite plain T-shirt colors. We promise not to judge if you prefer classic white.</p>
+                    <p>1.2 <strong>Contact Details:</strong> If you choose to contact us, we may collect your name and email address. We&apos;ll only use this information to respond to your inquiries and assist you with your plain shirt needs.</p>
+
+                    <h2 className="text-lg font-bold mt-4">2. How We Use Your Information</h2>
+                    <p>2.1 <strong>T-shirt Customization:</strong> We use the information you provide to personalize your shopping experience. Expect to see more plain T-shirts in colors you love!</p>
+                    <p>2.2 <strong>Communication:</strong> If you reach out to us, we&apos;ll use your contact details to respond promptly. No spam, just plain communication.</p>
+
+                    <h2 className="text-lg font-bold mt-4">3. Sharing Your Information</h2>
+                    <p>3.1 <strong>Plain Promise:</strong> We won&apos;t sell or share your information with third parties. Your T-shirt preferences are your business.</p>
+                    <p>3.2 <strong>Legal Obligations:</strong> In the event of legal requirements, we may disclose your information as necessary.</p>
+
+                    <h2 className="text-lg font-bold mt-4">4. Cookies and Similar Technologies</h2>
+                    <p>4.1 <strong>Cookie Comfort:</strong> We use cookies to enhance your shopping experience. Rest assured, they&apos;re as plain as our shirts.</p>
+                    <p>4.2 <strong>Opt-Out Options:</strong> If you&apos;re not a fan of cookies, you can adjust your browser settings to block them. However, some features may not work as expected.</p>
+
+                    <h2 className="text-lg font-bold mt-4">5. Your Privacy Choices</h2>
+                    <p>5.1 <strong>T-shirt Control:</strong> You can update your T-shirt preferences and contact details by contacting our customer service team.</p>
+                    <p>5.2 <strong>Unsubscribe:</strong> If you no longer want to receive plain updates from us, you can opt-out at any time. We won&apos;t take it personally.</p>
+
+                    <h2 className="text-lg font-bold mt-4">6. Security Measures</h2>
+                    <p>6.1 <strong>Secure Stitches:</strong> We take reasonable steps to protect your information. Our security measures are as solid as a well-stitched seam.</p>
+                    <p>6.2 <strong>No Shirt Left Behind:</strong> We do our best to ensure that your T-shirt preferences are safe and sound with us.</p>
+
+                    <p>By using PlainShirts, you acknowledge that you&apos;ve read our Privacy Policy and agree to let us know your favorite plain T-shirt colors. If you have any questions, our customer service team is standing by with non-serious responses and a lot of enthusiasm.</p>
+
+                    <p>Happy Plain Privacy!</p>
+                    <p>The PlainShirts Team</p>
+                </div>
+            </div>
         </div>
     );
 };
